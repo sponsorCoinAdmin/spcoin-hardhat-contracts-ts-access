5a25f55d338455db459b6da54e6507ce01422fac
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-11-22 04:22:15 -0500

    WIP

--- test/unit_TS_WETH/index.ts
+++ test/unit_TS_WETH/index.ts
@@ -5,11 +5,29 @@ import {assert} from 'chai';
 import { deployWETH9Contract } from "./lib.ts/hardhatSetup/deployContract";
 import { HhClassMethods } from "./lib.ts/hardhatSetup/hhClassMethods";
 
+
+// \Users\Robin\OneDrive\Dev\gitRepo\sponsorCoin\WIP\artifacts\contracts\WETH9.sol\WETH9.json
+
+import fs from 'fs';
+const fsPromises = fs.promises;
+
+
 let WETH9ContractDeployed:any;
 let hhClassMethods:any;
 let signer:any;
 let provider = hre.ethers.getDefaultProvider("mainnet");
 let ethers:any = hre.ethers;
+
+// load ABI from build artifacts
+async function getDeployedArtifactsAbi(contractName:string){
+  const HARDHAT_ARTIFACTS_HOME = "artifacts/contracts/";
+  const ABI_FILE_PATH=`${HARDHAT_ARTIFACTS_HOME}${contractName}.sol/${contractName}.json`
+  console.log(`ABI_FILE_PATH = ${ABI_FILE_PATH}`);
+  const data = await fsPromises.readFile(ABI_FILE_PATH, 'utf8');
+  const abi = JSON.parse(data)['abi'];
+  return abi;
+}
+
 
 describe("WETH9 Contract Deployed", function () {
   beforeEach(async () => {
@@ -24,7 +42,6 @@ describe("WETH9 Contract Deployed", function () {
     console.log(`signer.address = ${signer.address}`)
     // console.log("provider = " + JSON.stringify(provider,null,2));
 
-
     // Validate 20 HardHat Accounts created
     assert.equal(hhClassMethods.SPONSOR_ACCOUNT_SIGNERS.length, 20);
     // Validate Active signer Account is Account 0
@@ -33,14 +50,14 @@ describe("WETH9 Contract Deployed", function () {
     assert.equal(hhClassMethods.SPONSOR_ACCOUNT_SIGNERS[19], "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199");
   });
 
-  it("2. <TYPE SCRIPT> GET SIGNER BALANCE_OF and TEST ACCOUNT[5] BALANCE_OF", async function () {
+  xit("2. <TYPE SCRIPT> VALIDATE DEPLOYED CONTRACT BY GETTING SIGNER and TEST ACCOUNT[5] BALANCE_OF", async function () {
     const initialBalance:bigint = 10000000000000000000000n;
     const account5 = hhClassMethods.SPONSOR_ACCOUNT_SIGNERS[5];
     const signerBalance:bigint = await ethers.provider.getBalance(signer.address);
     const account5Balance:bigint = await ethers.provider.getBalance(account5);
 
-    // All Accounts have been given an ETH Balance of 10000000000000000000000 the signer account balance is less
-    // because gas was used for initial setup of hard hat
+    // All Accounts have been given an ETH Balance of 10000000000000000000000 except the signer account.
+    // The signer account (account[0]) balance is less because gas was used for initial setup of hard hat.
     console.log(`1. signer(${signer.address}) Balance = ${signerBalance}`);
     console.log(`2. account[5](${account5}) Balance = ${account5Balance}`);
 
@@ -48,50 +65,38 @@ describe("WETH9 Contract Deployed", function () {
     assert.equal(account5Balance, initialBalance);
   });
 
-  xit("3. <TYPE SCRIPT> GET SIGNER BALANCE_OF", async function () {
-    // console.log(`WETH9ContractDeployed = ${JSON.stringify(WETH9ContractDeployed,null,2
-    // hhClassMethods.dump()
+  it("3. <TYPE SCRIPT> Wrap ETH Using Sinner account[0]", async function () {
+    const initialBalance:bigint = 10000000000000000000000n;
+    let signerBalance:bigint = await ethers.provider.getBalance(signer.address);
+    const signedWeth = WETH9ContractDeployed.connect(signer);
+    const weth9_Address = WETH9ContractDeployed.address;
+    const wrapEthAmount = "2";
+    const wrapWeiAmount = 5;
 
-    const signedWeth = WETH9ContractDeployed.connect(signer)
+    const weth9ABI = await getDeployedArtifactsAbi("WETH9");
+    const WETH9Contract = new ethers.Contract(weth9_Address, weth9ABI, signer);
 
-    let account5 = hhClassMethods.SPONSOR_ACCOUNT_SIGNERS[5];
-    let signerBalance = await ethers.provider.getBalance(signer.address);
-    let account5Balance = await ethers.provider.getBalance(account5);
+    // console.log(`weth9_Address = ${weth9_Address}`);
+    // console.log(`weth9ABI      = ${JSON.stringify(weth9ABI,null,2)}`);
 
-    console.log(`1. signer(${signer.address}) Balance = ${signerBalance}`);
-    signerBalance = await ethers.provider.getBalance(signer.address);
-    console.log(`2. signer(${signer.address}) Balance = ${signerBalance}`);
-    console.log(`account[5](${account5}) Balance = ${account5Balance}`);
+    let signerWethBalance = await WETH9Contract.balanceOf(signer.address)
+    console.log(`1. BEFORE WRAP WETH9Contract signer($signerWethBalance}) Balance = ${signerWethBalance}`);
+
+    console.log(`2. BEFORE WRAP: signer(${signer.address}) Balance = ${signerBalance}`);
     // await this.WETH9ContractDeployed.connect(this.signer).deposit(account5, 1);
 
-    let val = await signedWeth.deposit({value: ethers.utils.parseEther("2")})
-    val = await signedWeth.deposit({value: 2})
-    console.log(`val = ${JSON.stringify(val,null,2)}`);
+    let tx = await WETH9Contract.deposit({value: ethers.utils.parseEther(wrapEthAmount)})
+    tx = await WETH9Contract.deposit({value: wrapWeiAmount})
+    // console.log(`tx = ${JSON.stringify(tx,null,2)}`);
     // await this.WETH9ContractDeployed.deposit(account5, 1);
     signerBalance = await ethers.provider.getBalance(signer.address);
-    console.log(`1. signer(${signer.address}) Balance = ${signerBalance}`);
+    console.log(`3. AFTER WRAP: signer(${signer.address}) Balance = ${signerBalance}`);
 
-    signerBalance = await ethers.provider.getBalance(signer.address);
-    console.log(`2. signer(${signer.address}) Balance = ${signerBalance}`);
-
-    let signerWethBalance = await signedWeth.balanceOf(signer.address)
-    console.log(`3. signer($signerWethBalance}) Balance = ${signerWethBalance}`);
+    signerWethBalance = await WETH9Contract.balanceOf(signer.address)
+    console.log(`4. AFTER WRAP WETH9Contract signer($signerWethBalance}) Balance = ${signerWethBalance}`);
 
     // let xxx = signerWethBalance+1
     // assert.equal(xxx, signerWethBalance);
   });   
 /**/
 });
-
-
-/*
-constructor(_WETH9ContractDeployed) {
-  this.WETH9ContractDeployed = _WETH9ContractDeployed;
-  WETH9Logger = new WETH9Logger(_WETH9ContractDeployed)
-  this.setSigner(_WETH9ContractDeployed.signer);
-}
-
-setSigner(_signer) {
-  this.signer = _signer;
-}
-*/
